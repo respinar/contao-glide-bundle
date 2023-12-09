b342aa33114e6c6878dda7b4ea44154332efd7f1
Hamid Peywasti <hamid@respinar.com>
2023-12-09 23:31:00 +0330

    disable services.yaml

--- src/DependencyInjection/RespinarContaoGlideExtension.php
+++ src/DependencyInjection/RespinarContaoGlideExtension.php
@@ -22,6 +22,6 @@ class RespinarContaoGlideExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-        $loader->load('services.yaml');
+        // $loader->load('services.yaml');
     }
 }
