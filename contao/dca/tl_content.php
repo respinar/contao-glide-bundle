3197e570fec3d7848c35e86598217feda564685c
Hamid Peywasti <hamid@respinar.com>
2024-11-18 14:48:40 +0330

    clean up

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -2,26 +2,16 @@
 
 declare(strict_types=1);
 
-use Respinar\ContaoGlideBundle\Controller\ContentElement\GlideStartController;
-use Respinar\ContaoGlideBundle\Controller\ContentElement\GlideStopController;
+use Respinar\ContaoGlideBundle\Controller\ContentElement\GlideController;
 
 /**
  * Modify the DCA for Glide
  */
 // Glide Wrapper (Start)
-$GLOBALS['TL_DCA']['tl_content']['palettes'][GlideStartController::TYPE] = '
-    {type_legend},type;
+$GLOBALS['TL_DCA']['tl_content']['palettes'][GlideController::TYPE] = '
+    {type_legend},type,headline;
+    {slider_legend},sliderDelay,sliderSpeed,sliderStartSlide,sliderContinuous;
     {template_legend:hide},customTpl;
     {protected_legend:hide},protected;
     {expert_legend:hide},cssID;
-    {invisible_legend:hide},invisible,start,stop
-';
-
-// Glide Wrapper (Stop)
-$GLOBALS['TL_DCA']['tl_content']['palettes'][GlideStopController::TYPE] = '
-    {type_legend},type;
-    {template_legend:hide},customTpl;
-    {protected_legend:hide},protected;
-    {expert_legend:hide},cssID;
-    {invisible_legend:hide},invisible,start,stop
-';
+    {invisible_legend:hide},invisible,start,stop';
